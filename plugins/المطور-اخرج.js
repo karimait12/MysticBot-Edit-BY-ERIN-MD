2b2288214b65325637f009a2e47cd67b87a97cdb
karimait12 <karimaityahia98@gmail.com>
2025-05-21 13:57:32 +0100

    Update المطور-اخرج.js

--- plugins/المطور-اخرج.js
+++ plugins/المطور-اخرج.js
@@ -1,7 +1,17 @@
-let handler = async (m, { conn, args, command }) => {
-await m.reply('*مش هتوحشوني مع السلامه اللي باعنا خسر دلعنا ⁦^⁠_⁠^⁩*') 
-await  conn.groupLeave(m.chat)}
-handler.command = /^(out|leavegc|اخرج|برا)$/i
+let handler = async (m, { conn, args, participants }) => {
+  if (!m.mentionedJid[0] && !m.quoted) throw '*منشن الشخص اللي عايز تطرده أو قم بالرد على رسالته*'
+  
+  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+  let isAdmin = participants.find(p => p.id === m.sender)?.admin === 'admin'
+  
+  if (!isAdmin) throw '*عذراً، هذا الأمر للمشرفين فقط!*'
+  
+  await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+  await m.reply(`*تم طرد العضو بنجاح* 🚷\n*مع السلامة يا @${user.split('@')[0]}*`, null, { mentions: [user] })
+}
+
+handler.command = /^(طرد|kick)$/i
 handler.group = true
-handler.rowner = true
+handler.admin = true
+handler.botAdmin = true
 export default handler
